4accd3d56b2deed4e04e662f285a67cf803fade5
anzunnt <67936366+anzunnt@users.noreply.github.com>
2024-03-12 05:13:46 +0700

    update manageUser

--- src/java/controller/ManageUserServlet.java
+++ src/java/controller/ManageUserServlet.java
@@ -4,7 +4,6 @@
  */
 package controller;
 
-import dal.VerifyCode;
 import dal.userDAO;
 import model.user;
 import java.io.IOException;
@@ -18,6 +17,7 @@ import jakarta.servlet.http.HttpSession;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import model.Admin;
 
 /**
  *
@@ -40,7 +40,15 @@ public class ManageUserServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try ( PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ManageUserServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ManageUserServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
         }
     }
 
@@ -56,19 +64,25 @@ public class ManageUserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String id_raw = request.getParameter("id");
-        int id;
-        userDAO ud = new userDAO();
+        HttpSession session = request.getSession();
+        Admin ad = (Admin) session.getAttribute("admin");
+        if (ad != null) {
+            String id_raw = request.getParameter("id");
+            int id;
+            userDAO ud = new userDAO();
 
-        if (id_raw != null) {
-            id = Integer.parseInt(id_raw);
-            user u = ud.GetUserById(id);
-            request.setAttribute("user", u);
-            request.setAttribute("doing", "Update");
-            request.getRequestDispatcher("manageUser.jsp").forward(request, response);
+            if (id_raw != null) {
+                id = Integer.parseInt(id_raw);
+                user u = ud.GetUserById(id);
+                request.setAttribute("user", u);
+                request.setAttribute("doing", "Update");
+                request.getRequestDispatcher("manageUser.jsp").forward(request, response);
+            } else {
+                request.setAttribute("doing", "Add");
+                request.getRequestDispatcher("manageUser.jsp").forward(request, response);
+            }
         } else {
-            request.setAttribute("doing", "Add");
-            request.getRequestDispatcher("manageUser.jsp").forward(request, response);
+            request.getRequestDispatcher("error.jsp").forward(request, response);
         }
     }
 
@@ -83,30 +97,26 @@ public class ManageUserServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession();
+        Admin ad = (Admin) session.getAttribute("account");
+
         try {
-            HttpSession session = request.getSession();
             String id = request.getParameter("id");
-            String fullname = request.getParameter("fullname");
-            String username = request.getParameter("username");
-            String password = request.getParameter("password");
-            String code = request.getParameter("code");
-            String email = request.getParameter("email");
-            String phone = request.getParameter("phone");
-            String image = request.getParameter("image");
-            String dob = request.getParameter("dob");
-            String address = request.getParameter("address");
-            String status = request.getParameter("status");
-            String created_on = request.getParameter("created_on");
+
+            userDAO ud = new userDAO();
 
             String currentDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.00").format(Calendar.getInstance().getTime());
 
-            userDAO ud = new userDAO();
             if (id != null) {
-                user u = new user(Integer.parseInt(id), fullname, username, password, code, email, phone, image, Date.valueOf(dob), address, (status.equals("active") ? 1 : 0), created_on, 1, 1, currentDate);
+                user u = ud.GetUserById(Integer.parseInt(id));
+                u.setStatus(Integer.parseInt(request.getParameter("status")));
+                u.setModified_on(currentDate);
+                u.setModified_by(ad.getId());
                 ud.UpdateUser(u);
                 response.sendRedirect("userlist");
             } else {
-                request.setAttribute("error", "User is not existed: " + username);
+                request.setAttribute("error", "User is not existed");
                 request.getRequestDispatcher("manageUser.jsp").forward(request, response);
             }
         } catch (Exception e) {
